d6f7587e18642b9fb38af37bc62cd674461f9e8f
KittiphonKamnuan <kittiphon.kam@dome.tu.ac.th>
2025-05-15 21:18:47 +0700

    Delete not User PaymentController

--- airline_booking_backend/src/main/java/com/airline/booking/controller/PaymentController.java
+++ airline_booking_backend/src/main/java/com/airline/booking/controller/PaymentController.java
@@ -15,7 +15,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.List;
